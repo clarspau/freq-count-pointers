ed4be6d300eb0cd1d7dcdf639896c73c4386967b
clarspau <125636195+clarspau@users.noreply.github.com>
2024-04-19 22:03:59 -0700

    construct-note.js

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,28 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+function constructNote(msg, letters) {
+     let msgFreq = {};
+     let lettersFreq = {};
+
+     // frequency counter for msg
+     for (let char of msg) {
+          msgFreq[char] = ++msgFreq[char] || 1;
+     }
+
+     // frequency counter for letters
+     for (char of letters) {
+          lettersFreq[char] = ++lettersFreq[char] || 1;
+     }
+
+     // check if msg can be constructed from letters
+     for (let char in msgFreq) {
+          // if there are not enough letters, then return false
+          if (!lettersFreq[char]) {
+               return false;
+          }
+          if (msgFreq[char] > lettersFreq[char]) {
+               return false;
+          }
+     }
+     return true;
+
 }
