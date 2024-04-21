b2a9eba3d5015d372afea9a2d2999e38ebf965d8
clarspau <125636195+clarspau@users.noreply.github.com>
2024-04-20 18:34:45 -0700

    longest-fall.js

--- longest-fall.js
+++ longest-fall.js
@@ -1,3 +1,22 @@
 // add whatever parameters you deem necessary
-function longestFall() {
+function longestFall(nums) {
+     let counter = 1;
+     let maxCounter = 1;
+
+     // if the array is empty, return 0
+     if (nums.length === 0) {
+          return 0;
+     }
+
+     for (let i = 1; i < nums.length; i++) {
+          // check if the current number is less than the previous number
+          if (nums[i] < nums[i - 1]) {
+               counter++;
+               maxCounter = Math.max(counter, maxCounter);
+          }
+          else {
+               counter = 1;
+          }
+     }
+     return maxCounter;
 }
