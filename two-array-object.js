6cf9510b3e226ae463c32258cd17c7716553fc03
clarspau <125636195+clarspau@users.noreply.github.com>
2024-04-21 18:11:07 -0700

    two-array-object.js

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,14 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keys, values) {
+     let result = {};
+
+     for (let i = 0; i < keys.length; i++) {
+          if (values[i] === undefined) {
+               result[keys[i]] = null;
+          }
+          else {
+               result[keys[i]] = values[i];
+          }
+     }
+     return result;
 }
