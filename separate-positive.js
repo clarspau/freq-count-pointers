288fc215adb1eaf895c7a43a7f2478664b42302b
clarspau <125636195+clarspau@users.noreply.github.com>
2024-04-21 17:49:26 -0700

    separate-positive.js

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,25 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(nums) {
+     let start = 0;
+     let end = nums.length - 1;
+
+     while (start < end) {
+          // if the start is negative and the end is positive, swap them
+          if (nums[start] < 0 && nums[end] > 0) {
+               [nums[start], nums[end]] = [nums[end], nums[start]];
+
+               // move the pointers
+               start++;
+               end--;
+          }
+          else {
+               if (nums[start] > 0) {
+                    start++;
+               }
+               else {
+                    end--;
+               }
+          }
+     }
+     return nums;
 }
